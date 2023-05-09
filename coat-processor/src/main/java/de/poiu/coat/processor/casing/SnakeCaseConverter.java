e993c4fdb6a7d978a595c09653ac7a6a2b9f9cd1
Marco Herrn <marco@mherrn.de>
2023-05-10 00:23:04 +0200

    Allow specifying casing strategy for inferred keys
    By default inferred keys have exactly the same case as the corresponding
    accessor. By specifying a different strategy they key can be written in
    snake_case or kebab-case.

---
+++ coat-processor/src/main/java/de/poiu/coat/processor/casing/SnakeCaseConverter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 - 2023 The Coat Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.poiu.coat.processor.casing;
+
+
+public class SnakeCaseConverter {
+
+  public static String convert(final String key) {
+    final StringBuilder sb= new StringBuilder();
+
+    for (final char c : key.toCharArray()) {
+      if (Character.isUpperCase(c)) {
+        sb.append("_");
+        sb.append(Character.toLowerCase(c));
+      } else {
+        sb.append(c);
+      }
+    }
+
+    return sb.toString();
+  }
+
+}
---
+++ coat-processor/src/test/java/de/poiu/coat/processor/casing/SnakeCaseConverterTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2020 - 2023 The Coat Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.poiu.coat.processor.casing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SnakeCaseConverterTest {
+
+
+  @Test
+  public void testConvert_allLowercase() {
+    assertThat(SnakeCaseConverter.convert("alllowercase")).isEqualTo("alllowercase");
+  }
+
+
+  @Test
+  public void testConvert_alreadySnakeCase() {
+    assertThat(SnakeCaseConverter.convert("already_snake_case")).isEqualTo("already_snake_case");
+  }
+
+
+  @Test
+  public void testConvert_camelCase() {
+    assertThat(SnakeCaseConverter.convert("camelCaseString")).isEqualTo("camel_case_string");
+  }
+
+
+  @Test
+  public void testConvert_multipleSubsequentUppercaseChars() {
+    assertThat(SnakeCaseConverter.convert("inAHurry")).isEqualTo("in_a_hurry");
+  }
+
+}
